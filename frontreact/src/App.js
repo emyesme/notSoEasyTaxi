739adda49bb00716b9b3fcd42501b350d461d0be
emyesme <emily.carvajal@correounivalle.edu.co>
2019-03-16 15:33:34 -0500

    /service component

--- frontreact/src/App.js
+++ frontreact/src/App.js
@@ -4,12 +4,12 @@ import CenterLogin from './components/centerLogin'
 import Menuser from './components/menuser';
 import Menudriver from './components/menudriver';
 import {Switch, Route, BrowserRouter} from 'react-router-dom'
-
+import Service from './components/service'
 
 class App extends Component {
-  //state = { modalShow: true };
+  state = { modalShow: true };
   render() {
-    //let modalClose = () => this.setState({ modalShow: false });
+    let modalClose = () => this.setState({ modalShow: false });
     return (
       <BrowserRouter>
       <Switch>
@@ -17,6 +17,7 @@ class App extends Component {
               component={() =>  <CenterLogin/>}/>
       <Route path='/menuser' component={() => <Menuser/>} />
       <Route path='/menudriver' component={() => <Menudriver/>} />
+      <Route path='/service' component={() => <Service show={this.state.modalShow} onHide={modalClose}/>}/>
       <Route path='*' component={Menuser} />
       </Switch>
     </BrowserRouter>
