b1e19bd0090fc600b94cbe631dcfa9f6f90b497d
Jaime1998 <jaime.cuartas@correounivalle.edu.co>
2019-03-16 17:29:04 -0500

    Con consulta de tipo localhost/client?cellphone=3107307371

--- back/index.js
+++ back/index.js
@@ -5,21 +5,37 @@ const app = myExpress()
 const port = 3000
 const base = myPgPromise('postgres://postgres:root@localhost:5432/postgres')
 
+app.get('/client', function(request, response){
+    const {cellphone} = request.query;
+    base.one('SELECT * FROM client WHERE cellphoneclient = $1 and status = true', [cellphone])
+    .then(function (dato){
+        console.log('Encontrado con exito \n')
+        console.log(dato)
+        response.send(dato)
+    })
+    .catch(function (error) {
+        console.log(error)
+        response.send({"error": {}})
+    })
+})
 
 
-app.get('/mapa' , function(request, response){
-    base.one('SELECT 123 AS a')
+app.get('/' , function(request, response){
+    base.any('SELECT * FROM client')
     .then( function (dato) {
         console.log(dato)
         response.send(dato)
     })
     .catch( function (error) {
         console.log(error)
+        response.send('error')
     })
 })
 
 
-app.listen(port)
+app.listen(port, () => {
+    console.log('Conección a la base de datos')
+})
 
 
 
